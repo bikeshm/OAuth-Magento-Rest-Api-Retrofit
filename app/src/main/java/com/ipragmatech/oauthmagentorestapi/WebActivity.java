eff98c741c257d8ee0b8cf58b998b7268aa69caf
ipragmatech <myor!@#4>
2015-07-13 15:08:16 +0530

    Initial Import

--- app/src/main/java/com/ipragmatech/oauthmagentorestapi/WebActivity.java
+++ app/src/main/java/com/ipragmatech/oauthmagentorestapi/WebActivity.java
@@ -1,13 +1,10 @@
 package com.ipragmatech.oauthmagentorestapi;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.net.UrlQuerySanitizer;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
@@ -29,13 +26,13 @@ import java.io.IOException;
 
 public class WebActivity extends BaseActivity {
 
-	private boolean handled;
+    private boolean handled;
     private WebView webView;
     private ProgressBar progress;
 
     @Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -46,60 +43,59 @@ public class WebActivity extends BaseActivity {
         progress.setIndeterminate(true);
         progress.setProgress(0);
 
-	}
-	
+    }
 
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		handled=false;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handled = false;
         new PreProcessToken().execute();
-        
-	}
-	
-	private class PreProcessToken extends AsyncTask<Uri, Void, Void> {
 
-		final OAuthHmacSigner signer = new OAuthHmacSigner();
-		private String authorizationUrl;
-		
-		@Override
-		protected Void doInBackground(Uri...params) {
+    }
 
-			try {
-			  
-		        signer.clientSharedSecret = Constants.CONSUMER_SECRET;
-		        
-				OAuthGetTemporaryToken temporaryToken = new OAuthGetTemporaryToken(Constants.REQUEST_URL);
-				temporaryToken.transport = new ApacheHttpTransport();
-				temporaryToken.signer = signer;
-				temporaryToken.consumerKey = Constants.CONSUMER_KEY;
-				temporaryToken.callback = Constants.OAUTH_CALLBACK_URL;
-				
-				OAuthCredentialsResponse tempCredentials = temporaryToken.execute();
-				signer.tokenSharedSecret = tempCredentials.tokenSecret;
-				
-				OAuthAuthorizeTemporaryTokenUrl authorizeUrl = new OAuthAuthorizeTemporaryTokenUrl(Constants.AUTHORIZE_URL);
-				authorizeUrl.temporaryToken = tempCredentials.token;
-				authorizationUrl = authorizeUrl.build();
-		        
-		        handled=false;
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		        
+    private class PreProcessToken extends AsyncTask<Uri, Void, Void> {
+
+        final OAuthHmacSigner signer = new OAuthHmacSigner();
+        private String authorizationUrl;
+
+        @Override
+        protected Void doInBackground(Uri... params) {
+
+            try {
+
+                signer.clientSharedSecret = Constants.CONSUMER_SECRET;
+
+                OAuthGetTemporaryToken temporaryToken = new OAuthGetTemporaryToken(Constants.REQUEST_URL);
+                temporaryToken.transport = new ApacheHttpTransport();
+                temporaryToken.signer = signer;
+                temporaryToken.consumerKey = Constants.CONSUMER_KEY;
+                temporaryToken.callback = Constants.OAUTH_CALLBACK_URL;
+
+                OAuthCredentialsResponse tempCredentials = temporaryToken.execute();
+                signer.tokenSharedSecret = tempCredentials.tokenSecret;
+
+                OAuthAuthorizeTemporaryTokenUrl authorizeUrl = new OAuthAuthorizeTemporaryTokenUrl(Constants.AUTHORIZE_URL);
+                authorizeUrl.temporaryToken = tempCredentials.token;
+                authorizationUrl = authorizeUrl.build();
+
+                handled = false;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
             return null;
-		}
+        }
 
-		
-		/**
-		 * When we're done and we've retrieved either a valid token or an error from the server,
-		 * we'll return to our original activity 
-		 */
-		@Override
-		protected void onPostExecute(Void result) {
 
-	        webView.setWebViewClient(new WebViewClient() {
+        /**
+         * When we're done and we've retrieved either a valid token or an error from the server,
+         * we'll return to our original activity
+         */
+        @Override
+        protected void onPostExecute(Void result) {
+
+            webView.setWebViewClient(new WebViewClient() {
 
                 @Override
                 public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -107,74 +103,74 @@ public class WebActivity extends BaseActivity {
                     super.onPageStarted(view, url, favicon);
                 }
 
-	        	@Override  
-	            public void onPageFinished(final WebView view, final String url)  {
+                @Override
+                public void onPageFinished(final WebView view, final String url) {
 
-	        		if (url.startsWith(Constants.OAUTH_CALLBACK_URL)) {
-		        		if (url.indexOf("oauth_token=")!=-1) {
-			        		webView.setVisibility(View.INVISIBLE);
-			        		
-			        		if (!handled) {
-			        			new ProcessToken(url,signer).execute();
-			        		}
-		        		} else {
-		        			webView.setVisibility(View.VISIBLE);
-		        		}
-	        		}
+                    if (url.startsWith(Constants.OAUTH_CALLBACK_URL)) {
+                        if (url.indexOf("oauth_token=") != -1) {
+                            webView.setVisibility(View.INVISIBLE);
+
+                            if (!handled) {
+                                new ProcessToken(url, signer).execute();
+                            }
+                        } else {
+                            webView.setVisibility(View.VISIBLE);
+                        }
+                    }
                     progress.setProgress(100);
                     progress.setVisibility(View.GONE);
-	            }
+                }
 
-	        });  
-	        
-	        webView.loadUrl(authorizationUrl);
+            });
 
-		}
+            webView.loadUrl(authorizationUrl);
 
-	}	
-	
-	private class ProcessToken extends AsyncTask<Uri, Void, Void> {
+        }
 
-		String url;
-		private OAuthHmacSigner signer;
-		
-		public ProcessToken(String url,OAuthHmacSigner signer) {
-			this.url=url;
-			this.signer = signer;
-		}
-		
-		@Override
-		protected Void doInBackground(Uri...params) {
+    }
 
-			Log.i(TAG, "doInbackground called with url " + url);
-			if (url.startsWith(Constants.OAUTH_CALLBACK_URL) && !handled) {
-        		try {
-					
-        			if (url.indexOf("oauth_token=")!=-1) {
-        				handled=true;
-            			String requestToken  = extractParamFromUrl(url,"oauth_token");
-            			String verifier= extractParamFromUrl(url,"oauth_verifier");
+    private class ProcessToken extends AsyncTask<Uri, Void, Void> {
+
+        String url;
+        private OAuthHmacSigner signer;
+
+        public ProcessToken(String url, OAuthHmacSigner signer) {
+            this.url = url;
+            this.signer = signer;
+        }
+
+        @Override
+        protected Void doInBackground(Uri... params) {
+
+            Log.i(TAG, "doInbackground called with url " + url);
+            if (url.startsWith(Constants.OAUTH_CALLBACK_URL) && !handled) {
+                try {
+
+                    if (url.indexOf("oauth_token=") != -1) {
+                        handled = true;
+                        String requestToken = extractParamFromUrl(url, "oauth_token");
+                        String verifier = extractParamFromUrl(url, "oauth_verifier");
 
                         OAuthGetAccessToken accessToken = getOAuthAccessToken(requestToken);
-            			accessToken.verifier = verifier;
+                        accessToken.verifier = verifier;
 
-            			OAuthCredentialsResponse credentials = accessToken.execute();
-            			signer.tokenSharedSecret = credentials.tokenSecret;
+                        OAuthCredentialsResponse credentials = accessToken.execute();
+                        signer.tokenSharedSecret = credentials.tokenSecret;
 
-						LocalCredentialStore localStore = new LocalCredentialStore(prefs);
-						localStore.store(new AuthToken(credentials.token, credentials.tokenSecret));
-			  		      
-        			} else if (url.indexOf("error=")!=-1) {
-        				new LocalCredentialStore(prefs).clear();
-        			}
-        			
-				} catch (IOException e) {
-					Log.e(TAG,e.getMessage(),e);
-				}
+                        LocalCredentialStore localStore = new LocalCredentialStore(prefs);
+                        localStore.store(new AuthToken(credentials.token, credentials.tokenSecret));
 
-        	}
+                    } else if (url.indexOf("error=") != -1) {
+                        new LocalCredentialStore(prefs).clear();
+                    }
+
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+
+            }
             return null;
-		}
+        }
 
         public OAuthGetAccessToken getOAuthAccessToken(String requestToken) {
             signer.clientSharedSecret = Constants.CONSUMER_SECRET;
@@ -186,28 +182,28 @@ public class WebActivity extends BaseActivity {
             return accessToken;
         }
 
-        private String extractParamFromUrl(String url,String paramName) {
+        private String extractParamFromUrl(String url, String paramName) {
 
-            Uri uri=Uri.parse(url);
+            Uri uri = Uri.parse(url);
 
-			return uri.getQueryParameter(paramName);
-		}   
-		
-		@Override
-		protected void onPreExecute() {
-			
-		}
+            return uri.getQueryParameter(paramName);
+        }
 
-		/**
-		 * When we're done and we've retrieved either a valid token or an error from the server,
-		 * we'll return to our original activity 
-		 */
-		@Override
-		protected void onPostExecute(Void result) {
-			startActivity(new Intent(WebActivity.this,MainActivity.class));
-			finish();
-		}
+        @Override
+        protected void onPreExecute() {
 
-	}	
+        }
+
+        /**
+         * When we're done and we've retrieved either a valid token or an error from the server,
+         * we'll return to our original activity
+         */
+        @Override
+        protected void onPostExecute(Void result) {
+            startActivity(new Intent(WebActivity.this, MainActivity.class));
+            finish();
+        }
+
+    }
 
 }
